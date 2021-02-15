6877a468cd244317e28454adce719851c303fada
Jorge <j_masson_reynoso@hotmail.com>
2021-02-15 01:03:32 -0700

    completo

--- app/src/main/java/masson/jorge/asignacion4_calculadoraimc_masson/MainActivity.kt
+++ app/src/main/java/masson/jorge/asignacion4_calculadoraimc_masson/MainActivity.kt
@@ -2,23 +2,26 @@ package masson.jorge.asignacion4_calculadoraimc_masson
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    var estatura: String = ""
-    var peso: String = ""
+    var estatura: Double = etEstatura.text.toString().toDouble()
+    var peso: Double = etPeso.text.toString().toDouble()
+    var resultado: String = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         btnCalcular.setOnClickListener {
-            
+            resultado = calcularIMC(estatura, peso).toString()
+            tvResultado.setText(resultado)
         }
     }
 
-    fun calcularIMC(estatura: Double, peso: Double): Double {
+    private fun calcularIMC(estatura: Double, peso: Double): Double {
         var imc = 0.0
 
         imc = peso / (estatura * estatura)
